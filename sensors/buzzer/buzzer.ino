6e970f5890d99851da266adfa63bc187a66494e7
Alissa Šubina <105237164+aalicc@users.noreply.github.com>
2022-08-12 21:05:18 +0300

    buzzer

---
+++ sensors/buzzer/buzzer.ino
@@ -0,0 +1,17 @@
+#include <Controllino.h>
+
+ const byte buzzer = 8;
+
+void setup() {
+  Serial.begin(9600);  
+  pinMode(buzzer, OUTPUT); 
+}
+
+void loop() {
+
+tone(buzzer, 400); //4000 in real life
+delay(500);
+
+noTone(buzzer);
+delay(500);
+}
